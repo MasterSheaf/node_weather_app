a61765211ea2842448fdf238e951194cfa27e0c7
Scott Sheaf <scottsheaf@gmail.com>
2021-11-28 22:09:40 -0500

    Complete first version of the integrated geolocation and weather functionality.

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("Weather app says hello");
+// const request = require('request');
+
+
+
+const geocode = require('./geocode');
+const weather = require('./weather');
+
+var myArgs = process.argv.slice(2);
+
+if (process.argv.length < 3) {
+    console.log("node app.js location");
+    process.exit();
+}
+
+// geocode an address
+geocode(process.argv[2], (error, geoData) => {
+    if (error) {
+        console.log("Geolocation Query Error ", error);
+    } else {
+
+        //console.log(`${geoData.location}`);
+        //console.log(`lat: ${geoData.latitude}`);
+        //console.log(`lon: ${geoData.longitude}`);
+
+        // now we can get the weather
+        weather(geoData.latitude, geoData.longitude, (error, weatherData) => {
+        
+            if (error) {
+
+                console.log("Weather Query Error ", error);
+
+            } else {
+
+                console.log(`In ${geoData.location} it is ${weatherData.temperature} deg and ${weatherData.description}`);
+
+            }
+        
+        });
+
+    }
+});
---
+++ weather.js
@@ -0,0 +1,65 @@
+const request = require('request')
+
+// const url = 'http://api.weatherstack.com/current?access_key=6ebcb1a94a8e01ad5aed80366d95ce80&query=43065&units=f';
+
+
+// request({url: url, json: true}, function (error, response, data) {
+
+//     if (error !== null) {
+//         console.error("error:", error); // Print the error if one occurred
+//         return;
+//     }
+
+//     console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+
+//     console.log("body:", data);
+//     console.log("Temp = " + data.current.temperature + " deg F");
+// });
+
+
+
+const weather = (lat, lon, callback) => {
+
+    var url = 'http://api.weatherstack.com/current?access_key=6ebcb1a94a8e01ad5aed80366d95ce80';
+    url += `&query=${lat},${lon}`;
+    url += '&units=f';
+
+    //console.log("URL = " + url);
+
+    //var url = 'http://api.weatherstack.com/current?access_key=6ebcb1a94a8e01ad5aed80366d95ce80&query=40.7831,-73.9712&units=f'
+
+    request({ url: url, json: true }, (error, response) => {
+
+        //console.log("Error: ", error);
+
+        //console.log(response.body);
+        
+        if (error) {
+
+            callback('Unable to connect to weather service', undefined)
+
+        } else if (response.body.success === false) {
+            
+            //console.log("Error Reported from API:");
+            //console.log(response.body.error);
+            callback("Error reported from API: " + response.body.error.code + ". " + response.body.error.info, undefined);
+        
+        } else {
+
+            if (response.body.current.weather_descriptions.length <=0) {
+                callback(undefined, {
+                    temperature: response.body.current.temperature,
+                    description: 'No Decsription Provided'
+                })
+            }
+            else {
+                callback(undefined, {
+                    temperature: response.body.current.temperature,
+                    description: response.body.current.weather_descriptions[0]
+                })
+            }
+        }
+    })
+}
+
+module.exports = weather
